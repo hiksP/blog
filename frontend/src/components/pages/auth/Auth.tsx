4e7b2c685ef4f7b126c1e1feb62e30fa749af01a
Plavskikh Peter <qksyo@yandex.ru>
2023-02-18 19:23:50 +0700

    custom input + button

--- frontend/src/components/pages/auth/Auth.tsx
+++ frontend/src/components/pages/auth/Auth.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 import styles from "./Auth.module.scss";
 import { Link } from "react-router-dom";
 import Layout from "../ui/Layout/Layout";
+import Input from "../ui/Input/Input";
+import Button from "../ui/Button/Button";
 
 const Auth: FC = () => {
   const isLogin = window.location.pathname === "/login";
@@ -11,11 +13,11 @@ const Auth: FC = () => {
       <section className={styles.auth}>
         <h2 className={styles.title}>{isLogin ? "Вход" : "Регистрация"}</h2>
         <form className={styles.form}>
-          <input className={styles.input} placeholder="Email"></input>
-          <input className={styles.input} placeholder="Пароль"></input>
-          <button type="submit" className={styles.button}>
+          <Input placeholder="Email"></Input>
+          <Input placeholder="Пароль"></Input>
+          <Button type="submit">
             {isLogin ? "Войти" : "Зарегестрироваться"}
-          </button>
+          </Button>
         </form>
         <Link to={isLogin ? "/register" : "/login"} className={styles.link}>
           {isLogin ? "Еще не зарегестрированы?" : "Уже зарегестрированы?"}
